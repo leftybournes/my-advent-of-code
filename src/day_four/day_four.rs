4dedd1df0a51f144b95f8f959f77cf935d19f616
Kent Delante <kdeleteme@tutanota.com>
2020-12-07 13:11:33 +0800

    Day four done

--- src/day_four/day_four.rs
+++ src/day_four/day_four.rs
@@ -24,25 +24,8 @@ impl Passport {
             cid: String::from(""),
         }
     }
-}
 
-fn part_one() {
-    let path = String::from("inputs/day_four/input.txt");
-    let input = inputs::parse_input(path).expect("Error parsing passport data");
-
-    let passports: Vec<String> = input
-        .split("\n\n")
-        .map(|passport| passport.parse().unwrap())
-        .collect();
-
-    let mut valid_passports = 0;
-
-    for passport_data in passports.iter() {
-        let passport_fields: Vec<String> = passport_data
-            .split_whitespace()
-            .map(|field| field.parse().unwrap())
-            .collect();
-
+    fn from_string_data(passport_fields: &Vec<String>) -> Self {
         let mut passport = Passport::new();
 
         for field in passport_fields.iter() {
@@ -61,6 +44,125 @@ fn part_one() {
                 _ => println!("{} field not applicable", field_pair[0]),
             }
         }
+
+        passport
+    }
+
+    fn validate(&self) -> bool {
+        self.validate_byr()
+            && self.validate_iyr()
+            && self.validate_eyr()
+            && self.validate_hgt()
+            && self.validate_hcl()
+            && self.validate_ecl()
+            && self.validate_pid()
+    }
+
+    fn validate_byr(&self) -> bool {
+        if self.byr.is_empty() {
+            return false;
+        }
+
+        let byr_len = self.byr.len();
+
+        let year = self.byr.parse::<u32>().unwrap();
+
+        byr_len == 4 && (1920..=2002).contains(&year)
+    }
+
+    fn validate_iyr(&self) -> bool {
+        if self.iyr.is_empty() {
+            return false;
+        }
+
+        let iyr_len = self.iyr.len();
+
+        let year = self.iyr.parse::<u32>().unwrap();
+
+        iyr_len == 4 && (2010..=2020).contains(&year)
+    }
+
+    fn validate_eyr(&self) -> bool {
+        if self.eyr.is_empty() {
+            return false;
+        }
+
+        let eyr_len = self.eyr.len();
+
+        let year = self.eyr.parse::<u32>().unwrap();
+
+        eyr_len == 4 && (2020..=2030).contains(&year)
+    }
+
+    fn validate_hgt(&self) -> bool {
+        if self.hgt.is_empty() {
+            return false;
+        }
+
+        let height = self
+            .hgt
+            .chars()
+            .filter(|c| ('0'..='9').contains(&c))
+            .collect::<String>()
+            .parse::<u32>()
+            .unwrap();
+
+        (self.hgt.contains("in") && (59..=76).contains(&height))
+            || (self.hgt.contains("cm") && (150..=193).contains(&height))
+    }
+
+    fn validate_hcl(&self) -> bool {
+        if self.hcl.is_empty() {
+            return false;
+        }
+
+        let hcl_len = self.hcl.len();
+        let first_char = &self.hcl[0..1];
+        let clr_code = &self.hcl[1..];
+
+        hcl_len == 7
+            && first_char == "#"
+            && clr_code.chars().fold(true, |acc, c| {
+                acc && (('0'..='9').contains(&c)
+                    || ('a'..='f').contains(&c)
+                    || ('A'..='F').contains(&c))
+            })
+    }
+
+    fn validate_ecl(&self) -> bool {
+        if self.ecl.is_empty() {
+            return false;
+        }
+
+        self.ecl == "amb"
+            || self.ecl == "blu"
+            || self.ecl == "brn"
+            || self.ecl == "gry"
+            || self.ecl == "grn"
+            || self.ecl == "hzl"
+            || self.ecl == "oth"
+    }
+
+    fn validate_pid(&self) -> bool {
+        if self.pid.is_empty() {
+            return false;
+        }
+
+        self.pid.len() == 9
+    }
+}
+
+fn part_one(passports: &Vec<String>) -> u32 {
+    let mut valid_passports: u32 = 0;
+
+    for passport_data in passports.iter() {
+        let passport_fields: Vec<String> = passport_data
+            .split_whitespace()
+            .map(|field| field.parse().unwrap())
+            .collect();
+
+        let passport = Passport::from_string_data(&passport_fields);
+
         if !passport.byr.is_empty()
             && !passport.iyr.is_empty()
             && !passport.eyr.is_empty()
@@ -73,9 +175,40 @@ fn part_one() {
         }
     }
 
-    println!("Valid passports: {}", valid_passports);
+    valid_passports
+}
+
+fn part_two(passports: &Vec<String>) -> u32 {
+    let mut valid_passports: u32 = 0;
+
+    for passport_data in passports.iter() {
+        let passport_fields: Vec<String> = passport_data
+            .split_whitespace()
+            .map(|field| field.parse().unwrap())
+            .collect();
+
+        let passport = Passport::from_string_data(&passport_fields);
+
+        if passport.validate() {
+            valid_passports += 1;
+        }
+    }
+
+    valid_passports
 }
 
 fn main() {
-    part_one();
+    let path = String::from("inputs/day_four/input.txt");
+    let input = inputs::parse_input(path).expect("Error parsing passport data");
+
+    let passports: Vec<String> = input
+        .split("\n\n")
+        .map(|passport| passport.parse().unwrap())
+        .collect();
+
+    let pone_valid_passports = part_one(&passports);
+    println!("Valid passports part one: {}", pone_valid_passports);
+
+    let ptwo_valid_passports = part_two(&passports);
+    println!("Valid passports part two: {}", ptwo_valid_passports);
 }
